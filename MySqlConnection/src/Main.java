09f3a456ca9b90376489605e7360711404ba038a
johnd65 <jedixon6565@gmail.com>
2013-02-04 14:01:14 -0500

    This updates login file
    This file has been modified to reflect the new login name and password
    for the database we can use to store our data. Please make note of it
    for use as needed.

--- MySqlConnection/src/Main.java
+++ MySqlConnection/src/Main.java
@@ -11,20 +11,21 @@ public static void main (String[] args)throws Exception
  Class.forName("com.mysql.jdbc.Driver");
  
  //Creating a variable for the connection called "con"
- Connection con = DriverManager.getConnection("jdbc:mysql://www.savianconsultants.com:3306/savianconsultants_com","a0000a6f","newlife2010" );
+ Connection con = DriverManager.getConnection("jdbc:mysql://www.savianconsultants.com:3306/savianconsultants_com_2","a0000a6f_1","century4last" );
+ //savianconsultants_com_2
+ //username = a0000a6f_1 password = century4last
  //jdbc:mysql://localhost:3306/
- //root is the database user
- //RicSpring13 is password 
+
  
  //Here we create our query
- PreparedStatement statement = con.prepareStatement(" SELECT * FROM names");
+ PreparedStatement statement = con.prepareStatement(" SELECT * FROM users");
  
  
  //Creating a variable to execute query
  ResultSet result = statement.executeQuery();
  
  while(result.next())
- {System.out.println(result.getString(1) + " " + result.getString(2));
+ {System.out.println(result.getString(1) + " " + result.getString(2) +" " + result.getString(3) + " " + result.getString(4));
  //getString returns the data
  //1 is the first field in the table
  //2 is the second field
